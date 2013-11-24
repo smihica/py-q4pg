83d95eed1fc17a53f44b9a3de140a4841a8a9469
smihica <smihica@gmail.com>
2013-11-24 23:30:56 +0900

    Updated version 0.0.8 -> 0.0.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 setup(
     name = 'py-q4pg',
     py_modules = ['q4pg'],
-    version = '0.0.8',
+    version = '0.0.9',
     description = 'A simple transactional message queue using PostgreSQL in Python.',
     author='Shin Aoyama',
     author_email = "smihica@gmail.com",
